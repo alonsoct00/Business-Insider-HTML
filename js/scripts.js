9b0f3a067dd95ce7f61293b91f64c450e8b74e1a
Alonso CT <g.alonso.ct@gmail.com>
2022-09-25 15:42:43 -0500

    ajustes comentarios wave 1

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "10/15/",
+        dayMonth = "10/06/",
         birthday = dayMonth + yyyy;
 
     today = mm + "/" + dd + "/" + yyyy;
